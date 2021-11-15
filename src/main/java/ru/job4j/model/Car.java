767de00b544ec806a5dbf790ae7bfc383462f071
BBergsJ <goldlike888@gmail.com>
2021-11-15 21:25:39 +0300

    Реализовать площадку продаж машин. [#4747]

--- src/main/java/ru/job4j/model/Car.java
+++ src/main/java/ru/job4j/model/Car.java
@@ -1,6 +1,7 @@
 package ru.job4j.model;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "cars")
@@ -11,4 +12,52 @@ public class Car {
 
     private String model;
     private String year;
+
+    public static Car of(String model, String year) {
+        Car car = new Car();
+        car.model = model;
+        car.year = year;
+        return car;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return id == car.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
